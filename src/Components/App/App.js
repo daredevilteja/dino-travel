c7b7c6818e68ffa752364cd4c9627f96c4375a5e
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-09-11 11:18:59 +0530

    Added login and signup handlers

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,123 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Login from "../Login";
-import SignUp from "../SignUp";
+import Home from "../Home";
 import "./App.css";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  return <div className="App">{isLoggedIn ? <Login /> : <SignUp />}</div>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [error, setError] = useState(undefined);
+  const [userName, setUserName] = useState(undefined);
+
+  const getUserName = () => {
+    return fetch(`http://localhost:9999/userInfo`, { credentials: "include" })
+      .then((r) => {
+        if (r.ok) {
+          return r.json();
+        } else {
+          setIsLoggedIn(false);
+          setUserName(undefined);
+          return { success: false };
+        }
+      })
+      .then((r) => {
+        if (r.success !== false) {
+          setIsLoggedIn(true);
+          setUserName(r.email);
+        }
+      });
+  };
+
+  useEffect(() => {
+    getUserName();
+  }, []);
+
+  const signupHandler = (user) => {
+    fetch("http://localhost:9999/signup", {
+      method: "POST",
+      body: JSON.stringify({
+        userName: user.userName,
+        email: user.email,
+        dob: user.dob,
+        password: user.password,
+        sex: user.sex,
+        country: user.country,
+        phNum: user.phNum,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    })
+      .then((r) => {
+        if (r.ok) {
+          return { success: true };
+        } else {
+          return r.json();
+        }
+      })
+      .then((r) => {
+        if (r.success === true) {
+          return logoutHandler();
+        } else {
+          setError(r.err);
+        }
+      });
+  };
+
+  const loginHandler = (userName, password) => {
+    login("http://localhost:9999/login", userName, password);
+  };
+
+  const logoutHandler = () => {
+    return fetch(`http://localhost:9999/logout`, {
+      credentials: "include",
+    }).then((r) => {
+      if (r.ok) {
+        setIsLoggedIn(false);
+        setUserName(undefined);
+      }
+    });
+  };
+
+  const login = (url, userName, password) => {
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ email: userName, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    })
+      .then((r) => {
+        if (r.ok) {
+          return { success: true };
+        } else {
+          return r.json();
+        }
+      })
+      .then((r) => {
+        if (r.success === true) {
+          return getUserName();
+        } else {
+          setError(r.err);
+        }
+      });
+  };
+
+  return (
+    <div className="App">
+      {!isLoggedIn ? (
+        <Login
+          isLoggedIn={setIsLoggedIn}
+          signupHandler={signupHandler}
+          loginHandler={loginHandler}
+          error={error}
+        />
+      ) : (
+        <Home username={userName} logoutHandler={logoutHandler} />
+      )}
+    </div>
+  );
 }
 
 export default App;
