1e8561096f9dee7dbbf8b54d8e6a18303a8c9b32
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-09-12 15:55:55 +0530

    Completed myTravels page functionality

--- src/Components/MyTravels/index.js
+++ src/Components/MyTravels/index.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "antd/dist/antd.css";
+import { Table, Typography } from "antd";
 
-export default function MyTravels(){
-    return (
-        <div><h1>MyTravels</h1></div>
-    );
+const { Title } = Typography;
+
+export default function MyTravels() {
+  const columns = [
+    {
+      title: "Start Location",
+      dataIndex: "startPoint",
+      key: "start",
+    },
+    {
+      title: "Destination",
+      dataIndex: "destination",
+      key: "destination",
+    },
+    {
+      title: "Dino",
+      dataIndex: "dino",
+      key: "dino",
+    },
+    {
+      title: "Start Time",
+      key: "startTime",
+      dataIndex: "startTime",
+    },
+    {
+      title: "End Time",
+      key: "endTime",
+      dataIndex: "endTime",
+    },
+  ];
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:9999/myTravels", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((r) => r.json())
+      .then((r) => setData([...r]));
+  }, []);
+
+  return (
+    <div>
+      <Title>Ongoing Rides</Title>
+      <Table columns={columns} dataSource={data} />
+    </div>
+  );
 }
